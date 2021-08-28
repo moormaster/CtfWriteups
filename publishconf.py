b84418ec8c75798b3b76e04f86cc77cd56fc625b
Andre Herbst <moormaster@gmx.net>
2021-08-29 01:55:09 +0200

    restructured menu items

--- publishconf.py
+++ publishconf.py
@@ -5,6 +5,7 @@
 # explicitly specify it as your config file.
 
 import os
+import re
 import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
@@ -17,6 +18,12 @@ FEED_ALL_ATOM = 'feeds/all.atom.xml'
 CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
 
 DELETE_OUTPUT_DIRECTORY = True
+
+# correct base location for links in menu item list
+menuitems = []
+for menuitem in MENUITEMS:
+    menuitems.append( ( menuitem[0], re.sub('^/', '/CtfWriteups/', menuitem[1]) ) )
+MENUITEMS = tuple(menuitems)
 
 # Following items are often useful when publishing
 
